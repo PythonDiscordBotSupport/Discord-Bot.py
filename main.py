6100926e23f3d5667c4994f825ebc0e707903fb9
PythonDiscordBotSupport <pydiscordbotsupport@gmail.com>
2026-08-16 02:58:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from core.connection import start_bot
+from web_server import keep_alive  # Импортируем из вашего web_server.py
 
 if __name__ == "__main__":
+    print("[System] Starting Flask web server...")
+    keep_alive()
+    
+    print("[System] Starting Discord bot...")
     start_bot()

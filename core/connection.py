42c0520466f8153bba14feceb56798a3b1980f6a
PythonDiscordBotSupport <pydiscordbotsupport@gmail.com>
2026-08-16 01:40:19 +0300

    Update connection.py

--- core/connection.py
+++ core/connection.py
@@ -1,1 +1,13 @@
+from config import bot_token
+import discord
+from discord.ext import commands
 
+intents = discord.Intents.default()
+intents.guilds = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+def start_bot():
+    # Запуск бота с токеном происходить ТОЛЬКО здесь
+    bot.run(bot_token)
+  

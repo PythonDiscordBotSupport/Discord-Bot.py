fd892fb3c79016374d668f93bf3bba70460e840f
PythonDiscordBotSupport <pydiscordbotsupport@gmail.com>
2026-08-16 03:17:30 +0300

    Update automation_core.py

--- automation/automation_core.py
+++ automation/automation_core.py
@@ -1,20 +1,32 @@
+import asyncio
 from discord.ext import commands, tasks
 from automation.division_information import update_division_stats
+from automation.server_members import update_server_members
 
 
 class AutomationCore(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        # Интервал цикла равен 5 минутам, так как 1 минута набегает внутри через asyncio.sleep
         self.automation_loop.start()
 
     def cog_unload(self):
         self.automation_loop.cancel()
 
-    @tasks.loop(minutes=6.0)
+    @tasks.loop(minutes=5.0)
     async def automation_loop(self):
-        # Вызов обновления дивизионов
+        # 1. Запуск обновления дивизионов
         await update_division_stats(self.bot)
+
+        # 2. Ждем 1 минуту перед запуском следующего модуля
+        await asyncio.sleep(60)
+
+        # 3. Запуск обновления участников сервера
+        await update_server_members(self.bot)
+
+        # После этого tasks.loop(minutes=5.0) подождет еще 5 минут.
+        # Итого суммарный перерыв между циклами = 6 минут.
 
     @automation_loop.before_loop
     async def before_automation_loop(self):
@@ -24,4 +36,4 @@ class AutomationCore(commands.Cog):
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(AutomationCore(bot))
-  
+    

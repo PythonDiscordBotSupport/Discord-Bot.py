22806f0e0c88fb63c47bac21ffb23fc6672ea191
PythonDiscordBotSupport <pydiscordbotsupport@gmail.com>
2026-09-09 13:10:52 +0300

    Update hr_slots.py

--- discord_commands/hr_slots.py
+++ discord_commands/hr_slots.py
@@ -14,7 +14,7 @@ class HRSlotsSetupCommand(commands.Cog):
     self.bot = bot
 
   @app_commands.command(
-      name="high ranks",
+      name="high-ranks",
       description="Creates an empty HR Slots embed template for future updates",
   )
   @app_commands.checks.has_permissions(administrator=True)

ad868c8d4933eb21300587c5cd1ff4b60198c05d
PythonDiscordBotSupport <pydiscordbotsupport@gmail.com>
2026-09-16 23:00:28 +0300

    Update xp_set.py

--- discord_commands/xp_set.py
+++ discord_commands/xp_set.py
@@ -16,7 +16,7 @@ from config import (
 SPREADSHEET_ID = "18v7NrP7-ipz6fBQ84yqvfrsIdfOao0EzUDrRQdokUX4"
 
 
-class XPCog(commands.Cog):
+class XPset(commands.Cog):
 
   def __init__(self, bot: commands.Bot):
     self.bot = bot
